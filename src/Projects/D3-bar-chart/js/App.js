ebf7eb2ad52ea8497fce26195383bac3f8a7a904
Pablussss <pablusprogramador@gmail.com>
2022-02-27 21:11:07 +0100

    continuacion d3

--- src/Projects/D3-bar-chart/js/App.js
+++ src/Projects/D3-bar-chart/js/App.js
@@ -38,19 +38,75 @@ let generateScales = () => {
         return new Date(item[0])
     })
 
-    console.log(datesArray)
-
     xAxisScale = d3.scaleTime()
-        .domain([])
-        .range()
+        .domain([d3.min(datesArray), d3.max(datesArray)])
+        .range([padding, width - padding])
+
+    yAxisScale = d3.scaleLinear()
+        .domain([0, d3.max(values, (item) => {
+            return item[1];
+        })])
+        .range([height - padding, padding])
 }
 
-let drawBars = () => {
+let drawBars =() => {
 
+    let tooltip = d3.select('body')
+                    .append('div')
+                    .attr('id', 'tooltip')
+                    .style('visibility', 'hidden')
+                    .style('width', 'auto')
+                    .style('height', 'auto')
+
+    svg.selectAll('rect')
+        .data(values)
+        .enter()
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('width', (width - (2 * padding)) / values.length)
+        .attr('data-date', (item) => {
+            return item[0]
+        })
+        .attr('data-gdp', (item) => {
+            return item[1]
+        })
+        .attr('height', (item) => {
+            return heightScale(item[1])
+        })
+        .attr('x', (item, index) => {
+            return xScale(index)
+        })
+        .attr('y', (item) => {
+            return (height - padding) - heightScale(item[1])
+        })
+        .on('mouseover', (item) => {
+            tooltip.transition()
+                .style('visibility', 'visible')
+
+            tooltip.text(item[0])
+            document.querySelector('#tooltip').setAttribute('data-date', item[0])
+        })
+        .on('mouseout', (item) => {
+            tooltip.transition()
+                .style('visibility', 'hidden')
+        })     
 }
+
 
 let generateAxes = () => {
 
+    let xAxis = d3.axisBottom(xAxisScale)
+    let yAxis = d3.axisLeft(yAxisScale)
+
+    svg.append('g')
+        .call(xAxis)
+        .attr("id", "x-axis")
+        .attr("transform", "translate(0, " + (height - padding) + ')')
+
+    svg.append('g')
+        .call(yAxis)
+        .attr("id", "y-axis")
+        .attr("transform", "translate(" + padding + ', 0)')
 }
 
 req.open('GET', url, true)
